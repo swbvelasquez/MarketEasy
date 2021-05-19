59c83d2d995bacaed0a24eeb19c6a40a53b7638d
swbvelasquez <swbvelasquez@gmail.com>
2021-05-18 19:56:10 -0500

    Added base to use handlers and threads

--- app/src/main/java/com/portfolio/marketeasy/core/global/Constants.java
+++ app/src/main/java/com/portfolio/marketeasy/core/global/Constants.java
@@ -3,4 +3,12 @@ package com.portfolio.marketeasy.core.global;
 public class Constants {
     public static final String DB_NAME="MarketEasy";
     public static final int DB_VERSION=1;
+
+    public static final int GETALL =1;
+    public static final int GETBYID =2;
+    public static final int INSERT=3;
+    public static final int UPDATE=4;
+    public static final int DELETE=5;
+    public static final int THREAD_ERROR=999;
+
 }
